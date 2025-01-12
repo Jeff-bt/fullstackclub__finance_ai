9fcc5873606865b49f2ea9d347a9a88c9dc90b5c
Jeff <jeffjunio.bt@gmail.com>
2025-01-11 21:17:34 -0300

    feat: add shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
 import Image from "next/image";
+import { Button } from "./_components/ui/button";
 
 export default function Home() {
-  return <div>Page Home</div>;
+  return (
+    <div>
+      <h1>Page Home</h1>
+      <Button>Sou um botão</Button>
+    </div>
+  );
 }
